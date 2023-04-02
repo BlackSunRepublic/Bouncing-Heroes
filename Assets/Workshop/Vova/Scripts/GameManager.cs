4f53e6cc734951a2f35496898e4e9a339fff2f1c
Vladimir Belousov <chexpk@gmail.com>
2023-04-02 20:57:19 +0600

    Фикс лидерборда

--- Assets/Workshop/Vova/Scripts/GameManager.cs
+++ Assets/Workshop/Vova/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -12,6 +13,8 @@ public class GameManager : MonoBehaviour
     [SerializeField] private List<LevelData> _levelsData;
     [Space(20)]
     [SerializeField] private TextMeshProUGUI _textMeshPro;
+    [SerializeField] private TextMeshProUGUI _textGlobalScore;
+    [SerializeField] private TextMeshProUGUI _textGlobalScoreInLeaderBoard;
     [SerializeField]
     private int _currentGameLevelNumberNotIndex = 1;
 
@@ -19,8 +22,30 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private GameObject mainMenuObject;
     [SerializeField] private GameObject _uiInGameObject;
+    [SerializeField] private GameObject _winPanel;
+    [SerializeField] private GameObject _playUI;
+    [Space(20)]
+    [SerializeField] private TextMeshProUGUI _textInLevelPlayUICoins;
+    private int _currentLevelCoins;
+    [SerializeField] private TextMeshProUGUI _textInLevelPlayUIRabs;
+    private int _currentLevelRabs;
+    [Space(20)] [Header("LEVEL WIN PANEL")]
+    [SerializeField] private TextMeshProUGUI _textInLevelWinPanelCoins;
+    [SerializeField] private TextMeshProUGUI _textInLevelWinPanelScore;
+
+    [SerializeField] private TextMeshProUGUI _textInLevelWinPanelRabs;
+    [SerializeField] private TextMeshProUGUI _textInLevelWinPanelRabsScore;
+
+    [SerializeField] private TextMeshProUGUI _textInLevelWinPanelGlobalScore;
+
+
 
     private Player _currentPlayer;
+
+    [SerializeField]
+    private int _multiplierCoinsToScore = 20;
+
+
 
     void Awake()
     {
@@ -34,15 +59,22 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
 
+    public void Start()
+    {
         UpdateUIStartScene();
+        Invoke(nameof(SendToLeaderBoard), 2f);
     }
 
     public void StartLevel(int index)
     {
-        //TODO check
+        //TODO check is level exist in LevelData !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
         _uiInGameObject.SetActive(true);
         mainMenuObject.SetActive(false);
+        _winPanel.SetActive(false);
+        CleanInPlayUi();
         SceneManager.LoadScene(index);
     }
 
@@ -55,25 +87,38 @@ public class GameManager : MonoBehaviour
 
     private void LevelFinish()
     {
+        _winPanel.SetActive(true);
+        _playUI.SetActive(false);
+        //TODO Update UI pnanel
+        AddToLevelDataResultOfLevel();
+        UpdateUiInWinPanel();
         Debug.Log("Level Finish");
+        SendToLeaderBoard();
     }
 
     private void UpdateUIStartScene()
     {
         if(_textMeshPro == null)
             return;
+
         _textMeshPro.text = _currentGameLevelNumberNotIndex.ToString();
+        _textGlobalScore.text = (GetAllCoinsCountFromAllLevels() * _multiplierCoinsToScore).ToString();
+        _textGlobalScoreInLeaderBoard.text = (GetAllCoinsCountFromAllLevels() * _multiplierCoinsToScore).ToString();
     }
 
     public void StartCurrentLevel()
     {
+        //TODO check is level exist in LevelData !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         _uiInGameObject.SetActive(true);
+        _winPanel.SetActive(false);
         //TODO Clean ALL UI
+        CleanInPlayUi();
         SceneManager.LoadScene(_currentGameLevelNumberNotIndex);
     }
 
     public void LoadMenu()
     {
+        UpdateUIStartScene();
         SceneManager.LoadScene(0);
     }
 
@@ -83,5 +128,61 @@ public class GameManager : MonoBehaviour
         UpdateUIStartScene();
     }
 
+    private void UpdateCoinsAdnRabsUi()
+    {
+        _textInLevelPlayUICoins.text = _currentLevelCoins.ToString();
+        _textInLevelPlayUIRabs.text = _currentLevelRabs.ToString();
+    }
+
+    public void AddCoins()
+    {
+        _currentLevelCoins++;
+        UpdateCoinsAdnRabsUi();
+    }
+
+    private void CleanInPlayUi()
+    {
+        _currentLevelCoins = 0;
+        _currentLevelRabs = 0;
+        UpdateCoinsAdnRabsUi();
+    }
+
+    private void AddToLevelDataResultOfLevel()
+    {
+        int levelCoinsResult = _currentLevelCoins - _currentLevelRabs;
+        if (levelCoinsResult < 0)
+            levelCoinsResult = 0;
+        if (_levelsData[_currentGameLevelNumberNotIndex - 1].MaximumPointsEarnedInLevel < levelCoinsResult)
+        {
+            _levelsData[_currentGameLevelNumberNotIndex - 1].MaximumPointsEarnedInLevel = levelCoinsResult;
+        }
+    }
+
+    private int GetAllCoinsCountFromAllLevels()
+    {
+        int result = 0;
+        foreach (var level in _levelsData)
+        {
+            result += level.MaximumPointsEarnedInLevel;
+        }
+
+        return result;
+    }
+
+    private void UpdateUiInWinPanel()
+    {
+        _textInLevelWinPanelCoins.text = _currentLevelCoins.ToString();
+        _textInLevelWinPanelScore.text = (_currentLevelCoins * _multiplierCoinsToScore).ToString();
+        _textInLevelWinPanelRabs.text = (-_currentLevelRabs).ToString();
+        _textInLevelWinPanelRabsScore.text = (-_currentLevelRabs * _multiplierCoinsToScore).ToString();
+
+        _textInLevelWinPanelGlobalScore.text = (GetAllCoinsCountFromAllLevels() * _multiplierCoinsToScore).ToString();
+    }
+
+    public void SendToLeaderBoard()
+    {
+        int result = GetAllCoinsCountFromAllLevels() * _multiplierCoinsToScore;
+        gameObject.GetComponent<PlayFabManager>().SendLeaderboard(result);
+    }
 
 }
--- Assets/Workshop/Vova/Scripts/Player/PlayerCoinCollector.cs
+++ Assets/Workshop/Vova/Scripts/Player/PlayerCoinCollector.cs
@@ -4,15 +4,12 @@ namespace Workshop
 {
     public class PlayerCoinCollector : MonoBehaviour
     {
-        [SerializeField]
-        private CoinCounter _coinCounter;
-
         private void OnTriggerEnter2D(Collider2D colCoin)
         {
             Coin _coin = colCoin.GetComponent<Coin>();
             if (_coin != null)
             {
-                _coinCounter.AddCoin();
+                GameManager.instance.AddCoins();
                 Destroy(colCoin.gameObject);
             }
         }
